e10b49769867b26193d81dab8690ad327b7cc577
Ivan Litov <ivanslitov@gmail.com>
2021-05-26 17:31:52 +0300

    Add tasks executor wrappers.

---
+++ CustomLibraries/MessagesTaskRunnerLibrary/MessagesTaskRunner.h
@@ -0,0 +1,26 @@
+#ifndef _MESSAGES_TASK_RUNNER_
+#define _MESSAGES_TASK_RUNNER_
+
+#include <array>
+#include <thread>
+
+#include "../MessagesProcessor/MessagesProcessor.h"
+
+class MessagesTaskRunner {
+public:
+	// Two tasks: 1 for the local messages and 1 for the messages that we receive.
+	static const int MAX_TASK_COUNT = 2;
+	MessagesTaskRunner();
+	MessagesTaskRunner(const MessagesTaskRunner&) = delete;
+	MessagesTaskRunner& operator=(const MessagesTaskRunner&) = delete;
+	~MessagesTaskRunner();
+
+	bool push(MessagesProcessorBase *task);
+	void joinTasks();
+
+private:
+	int m_index;
+	std::array<std::thread, MAX_TASK_COUNT> m_tasks;
+};
+
+#endif // !_MESSAGES_TASK_RUNNER_
