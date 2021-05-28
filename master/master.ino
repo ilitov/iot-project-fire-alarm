223a99c2489d9f83555b48c964b32523a1d25b91
Ivan Litov <ivanslitov@gmail.com>
2021-05-28 20:30:09 +0300

    Make the settings of master configurable from the user.

--- master/master.ino
+++ master/master.ino
@@ -1,6 +1,9 @@
 #include <WiFi.h>
+#include <WebServer.h>
 #include <esp_wifi.h>
+
 #include "EspNowManager.h"
+#include "EspSettings.h"
 
 const unsigned char SlaveMAC[6] = {0x7C,0x9E,0xBD,0xED,0x95,0xF4}; 
 const bool isMaster = true;
@@ -8,7 +11,10 @@ const bool isMaster = true;
 static EspNowManager &espman = EspNowManager::instance();
 static MasterProcessorPeers peersProcessor{MasterCallbackPeers{espman}};
 
+static ESPSettings &espSettings = ESPSettings::instance();
+
 void setupWiFi(){
+  WiFi.disconnect();
   WiFi.mode(WIFI_STA);
   WiFi.beginSmartConfig();
   //WiFi.begin("MY_WIF_SSID", "PASSWORD");
@@ -36,17 +42,36 @@ void setupWiFi(){
   // This is needed if we are not in AP mode.
   WiFi.setSleep(WIFI_PS_NONE);
   //esp_wifi_set_ps(WIFI_PS_NONE);
-}
 
-void setup() {
-  Serial.begin(115200);
-  setupWiFi();
-
+  // This channel must be sent to slaves through the WebServer.
   uint32_t wifiChannel = WiFi.channel();
   Serial.print("WiFi channel: ");
   Serial.println(wifiChannel);
 
-  if(false == espman.init(&peersProcessor, NULL, wifiChannel, isMaster, WiFi.macAddress().c_str())){
+  if(!espSettings.setESPNowChannel(wifiChannel)){
+    Serial.println("The WiFi channel is not appropriate! Restarting...");
+    esp_restart();
+  }
+
+  // Save the settings to the internal memory.
+  espSettings.updateSettings();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Setup the file system and ESP settings.
+  if(!espSettings.setup()){
+    esp_restart();
+  }
+
+  // Setup a phone number and credentials.
+  espSettings.setupUserSettings();
+
+  // Onboard the ESP with SmartConfig.
+  setupWiFi();
+
+  if(false == espman.init(&peersProcessor, NULL, espSettings.getESPNowChannel(), isMaster, WiFi.macAddress().c_str())){
     Serial.println("Failed to init ESP-NOW, restarting!");
     esp_restart();
   }
